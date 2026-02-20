45404d08cf29b379d53fd572e5911cea69828939
joanveras <verasjoan587@gmail.com>
2026-02-20 03:18:13 -0300

    fix: add missing `logging` middleware

--- backend/shared/shared/middlewares/__init__.py
+++ backend/shared/shared/middlewares/__init__.py
@@ -1,4 +1,5 @@
 from .auth import auth_middleware
+from .logging import logging_middleware
 from .request_context import request_context_middleware
 
-__all__ = ["auth_middleware", "request_context_middleware"]
+__all__ = ["auth_middleware", "request_context_middleware", "logging_middleware"]
