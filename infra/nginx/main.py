439d2ef27e18bc3b9e24b4f3ab9468f7c95e8469
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2026-02-21 22:32:09 -0300

    docs: add backend docs in nginx

--- infra/nginx/main.py
+++ infra/nginx/main.py
@@ -25,10 +25,10 @@ app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.VERSION,
     lifespan=lifespan,
-    root_path="/api",
-    docs_url="/tournaments/docs",
-    redoc_url="/tournaments/redoc",
-    openapi_url="/tournaments/openapi.json",
+    root_path="/api/tournaments",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json",
 )
 
 app.add_exception_handler(DomainError, app_exception_handler)
