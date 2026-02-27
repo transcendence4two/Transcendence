7528f6ba4ed7c71e72b87fde8864677c37c1468b
dwbessa <danielbessa01@gmail.com>
2026-02-26 23:44:52 -0300

    feat: added /verify-otp route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage'
 import PrivacyPolicy from './pages/PrivacyPolicy'
 import RegisterPage from './pages/RegisterPage'
 import LoginPage from './pages/LoginPage'
+import TFAPage from './pages/TFAPage'
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />"
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/verify-otp" element={<TFAPage />} />
       </Routes>
     </BrowserRouter>
   )
