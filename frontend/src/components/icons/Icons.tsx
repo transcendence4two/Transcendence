5046e149891f55dc9b25ec154d208b108375434f
dwbessa <danielbessa01@gmail.com>
2026-03-03 11:42:46 -0300

    feat: new icons for new pages

--- frontend/src/components/icons/Icons.tsx
+++ frontend/src/components/icons/Icons.tsx
@@ -91,3 +91,9 @@ export const GoogleIcon = ({ className = 'w-5 h-5' }: IconProps) => (
         <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
     </svg>
 )
+
+export const HomeIcon = ({ className = 'w-5 h-5' }: IconProps) => (
+    <svg className={className} fill='none' stroke='currentColor' viewBox='0 0 24 24'>
+        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' />
+    </svg>
+)
