16a746d8518f95195d9b5d22ea0a6c2d561a3570
joanveras <verasjoan587@gmail.com>
2026-03-05 16:04:05 -0300

    feat: made a component from `ProfilePage`

---
+++ frontend/src/components/common/ProfileHelpFab.tsx
@@ -0,0 +1,21 @@
+import { QuestionLgIcon } from "../icons/Icons";
+
+type ProfileHelpFabProps = {
+  ariaLabel?: string;
+  onClick?: () => void;
+};
+
+const ProfileHelpFab = ({ ariaLabel = "Help", onClick }: ProfileHelpFabProps) => {
+  return (
+    <button
+      type="button"
+      className="profile-help-fab"
+      aria-label={ariaLabel}
+      onClick={onClick}
+    >
+      <QuestionLgIcon className="profile-help-icon icon-white" />
+    </button>
+  );
+};
+
+export default ProfileHelpFab;
