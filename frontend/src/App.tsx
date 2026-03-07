f40b70d6d2b28f5c7e070594d0a2360b89ebe80f
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2026-03-06 22:09:38 -0300

    fix app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import PrivacyPolicy from './pages/PrivacyPolicy'
-import RegisterPage from './pages/RegisterPage'
-import LoginPage from './pages/LoginPage'
-import TFAPage from './pages/TFAPage'
-import DashboardPage from './pages/DashboardPage'
-import MatchmakingPage from './pages/MatchmakingPage'
-import GamePage from './pages/GamePage'
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
@@ -16,8 +5,9 @@ import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import TFAPage from "./pages/TFAPage";
 import DashboardPage from "./pages/DashboardPage";
+import MatchmakingPage from "./pages/MatchmakingPage";
+import GamePage from "./pages/GamePage";
 import ProfilePage from "./pages/ProfilePage";
->>>>>>> main
 
 function App() {
   return (
@@ -29,20 +19,12 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/verify-otp" element={<TFAPage />} />
         <Route path="/home" element={<DashboardPage />} />
-<<<<<<< HEAD
         <Route path="/matchmaking" element={<MatchmakingPage />} />
         <Route path="/game/:sessionId" element={<GamePage />} />
-=======
         <Route path="/profile" element={<ProfilePage />} />
->>>>>>> main
-      </Routes >
-    </BrowserRouter >
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-<<<<<<< HEAD
-export default App
-
-=======
 export default App;
->>>>>>> main
