d383422851a6a9f6cbbee01c99a52726e78f24ec
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2026-03-10 23:35:08 -0300

    att

--- backend/friends-service/src/test/java/br/com/transcendence/GreetingResourceIT.java
+++ backend/friends-service/src/test/java/br/com/transcendence/GreetingResourceIT.java
@@ -3,6 +3,4 @@ package br.com.transcendence;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
+class GreetingResourceIT extends GreetingResourceTest {}
