f50b3630a319b53bffc510fb1d210c2b0762ed59
joanveras <verasjoan587@gmail.com>
2026-03-13 14:41:41 -0300

    refactor: rename ProfileHelpFab to HelpFab

---
+++ frontend/src/components/common/HelpFab.tsx
@@ -0,0 +1,21 @@
+import { QuestionLgIcon } from "../icons/Icons";
+
+type ProfileHelpFabProps = {
+  ariaLabel?: string;
+  onClick?: () => void;
+};
+
+const HelpFab = ({ ariaLabel = "Help", onClick }: ProfileHelpFabProps) => {
+  return (
+    <button
+      type="button"
+      className="help-fab"
+      aria-label={ariaLabel}
+      onClick={onClick}
+    >
+      <QuestionLgIcon className="profile-help-icon icon-white" />
+    </button>
+  );
+};
+
+export default HelpFab;
