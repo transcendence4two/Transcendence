af686aa6df0653069f5d4e5c9ce8e39c669e7c05
dwbessa <danielbessa01@gmail.com>
2026-03-15 02:15:43 -0300

    fix(friends-service): validate users via internal exists endpoint

--- backend/friends-service/src/main/java/br/com/transcendence/friends/infrastructure/client/IUserManagementClient.java
+++ backend/friends-service/src/main/java/br/com/transcendence/friends/infrastructure/client/IUserManagementClient.java
@@ -1,22 +1,16 @@
 package br.com.transcendence.friends.infrastructure.client;
 
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-@Path("/users")
 @RegisterRestClient(configKey = "usermanagement-client")
 @RegisterClientHeaders(HeaderPropagationFactory.class)
 public interface IUserManagementClient {
 
     @GET
-    @Path("/find-by-username/{username}")
-    Response findByUsername(@PathParam("username") String username);
-
-    @GET
-    @Path("/{id}")
-    Response findById(@PathParam("id") String id);
+    @jakarta.ws.rs.Path("/internal/users/{id}/exists")
+    Response userExists(@PathParam("id") String id);
 }
